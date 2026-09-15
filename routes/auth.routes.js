604f309cf8103107a4ea8d1a5db89d9f09f2d0e2
Maicol <mamian642@gmail.com>
2026-09-15 11:57:41 -0500

    Se limpia lo que no se usa

--- routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/auth.controller');
-
-router.get('/google', authController.redirectToGoogle);
-router.get('/google/callback', authController.handleCallback);
-router.get('/accounts', authController.listAccounts);
-
-module.exports = router;
